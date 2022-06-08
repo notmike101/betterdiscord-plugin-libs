1830f1b14d7d1d22e6fd82f6cb184513a0f216bf
Mike Orozco <me@mikeorozco.dev>
2022-06-07 17:12:01 -0700

    Add new developerMode DiscordModule

--- src/libs/DiscordModules.ts
+++ src/libs/DiscordModules.ts
@@ -3,9 +3,11 @@ import { findModuleByProps } from 'betterdiscord/bdapi';
 export const selectedGuildStore = findModuleByProps('getLastSelectedGuildId');
 export const guildStore = findModuleByProps('getGuilds');
 export const app = findModuleByProps('app', 'layers');
+export const developerMode = findModuleByProps('DeveloperMode');
 
 export const DiscordModules = Object.freeze({
   selectedGuildStore,
   guildStore,
   app,
+  developerMode,
 });
